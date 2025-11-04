11d9dbdefb5f40ffd8892bffe5c2dcad9dc74c07
Peter Kaiser <peterkaiserui@gmail.com>
2025-11-03 20:51:39 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/gaze-genius-lab/",
+  base: mode === "production" ? "/gaze-genius-lab/" : "/",
   server: {
     host: "::",
     port: 8080,
