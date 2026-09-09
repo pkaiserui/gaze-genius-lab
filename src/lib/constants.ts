0f126b7bffacd18718018ae6f9adac87238a19c9
Peter Kaiser <peter@kaiser.ai>
2026-09-09 11:01:26 -0500

    uipdate link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 // Shared constants for the app
-export const calendarLink = "https://calendar.app.google/eERzu3NGVVtWQLEP7";
+export const calendarLink = "https://calendar.app.google/RHAUUZt6c9GraNhQ9";
